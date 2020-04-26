27c5aeffeebec0281b78bc068fbd48583d008fb4
unknown <jaylord.abueva98@gmail.com>
2020-04-26 17:20:47 +0800

    Jay First Commit

---
+++ build/precache-manifest.28e948bc5c936f020de65b6fa59c5686.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47768889a7a98e3b9ec8609e3852f49a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2462832d1986fdf2b14",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "87f1a71f96466416d455",
+    "url": "/static/css/main.3c734533.chunk.css"
+  },
+  {
+    "revision": "e2462832d1986fdf2b14",
+    "url": "/static/js/2.df82e802.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.df82e802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f1a71f96466416d455",
+    "url": "/static/js/main.9db6db62.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "175db1f7e587c789f1a4b719c2e98dfe",
+    "url": "/static/media/1.175db1f7.png"
+  },
+  {
+    "revision": "129017a9cd970e3c4a0dada0731b31fe",
+    "url": "/static/media/2.129017a9.png"
+  },
+  {
+    "revision": "0436cfc6498cc5d5ad0d63139dcb4b95",
+    "url": "/static/media/3.0436cfc6.png"
+  },
+  {
+    "revision": "0436cfc6498cc5d5ad0d63139dcb4b95",
+    "url": "/static/media/4.0436cfc6.png"
+  },
+  {
+    "revision": "8fcfdaa54992cdef5dc5c76409e4889b",
+    "url": "/static/media/banner.8fcfdaa5.png"
+  },
+  {
+    "revision": "6dd79a4ed4122fc8107dd1ff3633da19",
+    "url": "/static/media/home-bg-banner.6dd79a4e.png"
+  },
+  {
+    "revision": "753ff9bbddcd9cc7d65482d3684f3467",
+    "url": "/static/media/logo-shadow.753ff9bb.png"
+  }
+]);
